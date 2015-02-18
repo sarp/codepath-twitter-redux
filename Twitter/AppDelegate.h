eb97ec454a2c1d1e251f62862f5d2a167fa19604
Sarp Centel <sarp@fb.com>
2015-02-17 19:13:02 -0500

    Initial Commit

---
+++ Twitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Twitter
+//
+//  Created by Sarp Centel on 2/17/15.
+//  Copyright (c) 2015 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Twitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Twitter
+//
+//  Created by Sarp Centel on 2/17/15.
+//  Copyright (c) 2015 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
